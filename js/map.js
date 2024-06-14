2ab5dc446968a89aa2b418e82a34d40b020bdc88
TuGitee <tgb@std.uestc.edu.cn>
2024-06-15 00:53:29 +0800

    first commit

---
+++ js/map.js
@@ -0,0 +1,40 @@
+const storeList = document.getElementById('store-list');
+
+function init(geoList) {
+    for (let i = 0; i < geoList.length; i++) {
+        const dom = document.getElementById('map' + i);
+        const center = new qq.maps.LatLng(geoList[i].geo[0], geoList[i].geo[1]);
+        const map = new qq.maps.Map(dom, {
+            center,
+            zoom: 17,
+            draggable: true,
+            scrollwheel: true,
+        });
+
+        const marker = new qq.maps.Marker({
+            position: center,
+            map,
+            title: geoList[i].name,
+            animation: qq.maps.MarkerAnimation.BOUNCE,
+            clickable: true,
+        })
+    }
+}
+
+fetch('./data/geo.json').then(res => res.json()).then(data => {
+    const geoList = data;
+    storeList.innerHTML = geoList.map((item, index) => {
+        return `<div class="store">
+        <div class="image">
+            <div id="map${index}"></div>
+        </div>
+        <div class="text">
+            <h4>${item.name}</h4>
+            <p>地址：${item.address}</p>
+            <p>电话：<a href="tel:${item.phone}">${item.phone}</a></p>
+            <p>营业时间：${item.time}</p>
+        </div>
+    </div>`
+    }).join('')
+    init(geoList)
+})
